172d05d180281c70f85445085a2bf029b4128adf
n14011 <n14011@std.it-college.ac.jp>
2014-10-21 11:49:43 +0900

    1021

--- Ex2.java
+++ Ex2.java
@@ -52,15 +52,72 @@ class Ex2_2 extends Ex{
         System.out.println("合計は" + (x + y + z) + "です。");
         System.out.println("平均は" + (x + y + z) / 3 + "です。");
     }
-
-
-
-
-
-
-
 }
 
+class Ex2_3 extends Ex{
+    void execute(){
+ Scanner stdIn = new Scanner(System.in);
+
+
+        System.out.print("整数値：");// xの値の入力を促す
+        int x = stdIn.nextInt();
+
+        System.out.println(x);
+
+
+    }
+}
+class Ex2_4 extends Ex{
+    void execute(){
+        Scanner stdIn = new Scanner(System.in);
+
+
+        System.out.print("xの値：");// xの値の入力を促す
+        int x = stdIn.nextInt();
+
+
+        System.out.println("x + 10 = " + (x + 10)); // x + yの値を表示
+        System.out.println("x - 10 = " + (x - 10)); // x - yの値を表示
+
+
+    }
+}
+class Ex2_5 extends Ex{
+    void execute(){
+        Scanner stdIn = new Scanner(System.in);
+
+
+        System.out.print("xの値：");// xの値の入力を促す
+        int x = stdIn.nextInt();
+
+        System.out.print("yの値：");// yの値の入力を促す
+        int y = stdIn.nextInt();
+
+        System.out.println("和:" + (x + y)); // x + yの値を表示
+        System.out.println("平均: " + (x + y)/2); // x - yの値を表示
+
+
+    }
+}
+
+class Ex2_6 extends Ex{
+    void execute(){
+
+        Scanner stdIn = new Scanner(System.in);
+
+
+        System.out.print("底辺：");// xの値の入力を促す
+        double x = stdIn.nextInt();
+
+        System.out.print("高さ：");// yの値の入力を促す
+        double y = stdIn.nextInt();
+
+
+        System.out.println("面積 " + (x * y)/2); // x * yの値を表示
+
+
+    }
+}
 
 public class Ex2 {
     public static void main(String[] args){
@@ -71,37 +128,44 @@ public class Ex2 {
         a.execute();
         a = new Ex2_2();
         a.execute();
+        a = new Ex2_3();
+        a.execute();
+        a = new Ex2_4();
+        a.execute();
+        a = new Ex2_5();
+        a.execute();
+        a = new Ex2_6();
+        a.execute();
+
     }
 }
 
 class Ex2_00 extends Ex{
-	void execute(){
-		Scanner stdIn = new Scanner(System.in);
+    void execute(){
+        Scanner stdIn = new Scanner(System.in);
 
-		System.out.println("xとyを加減乗除します。");
+        System.out.println("xとyを加減乗除します。");
 
-		System.out.print("xの値：");		// xの値の入力を促す
-		int x = stdIn.nextInt();
+        System.out.print("xの値：");// xの値の入力を促す
+        int x = stdIn.nextInt();
 
-		System.out.print("yの値：");		// yの値の入力を促す
-		int y = stdIn.nextInt();
+        System.out.print("yの値：");// yの値の入力を促す
+        int y = stdIn.nextInt();
 
-		System.out.println("x + x = " + (x + y)); // x + yの値を表示
-		System.out.println("x - x = " + (x - y)); // x - yの値を表示
-		System.out.println("x * x = " + (x * y)); // x * yの値を表示
-		System.out.println("x / x = " + (x / y)); // x / yの値を表示 (商)
-		System.out.println("x % x = " + (x % y)); // x % yの値を表示 (剰余)
-	}
+        System.out.println("x + x = " + (x + y)); // x + yの値を表示
+        System.out.println("x - x = " + (x - y)); // x - yの値を表示
+        System.out.println("x * x = " + (x * y)); // x * yの値を表示
+        System.out.println("x / x = " + (x / y)); // x / yの値を表示 (商)
+        System.out.println("x % x = " + (x % y)); // x % yの値を表示 (剰余)
+    }
 }
 
 class Ex2_000 extends Ex{
-	void execute(){
-		Random rand = new Random();
+    void execute(){
+        Random rand = new Random();
 
-		int luchy = rand.nextInt(10);		// 0～9の乱数
-
-		System.out.println("今日のラッキーナンバーは" + luchy + "です。");
-	}
+        int luchy = rand.nextInt(10);// 0～9の乱数
+        System.out.println("今日のラッキーナンバーは" + luchy + "です。");
+    }
 }
-
 
