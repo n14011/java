3726ce06da870ea3d2af07d401e97ccd706da03c
n14011 <n14011@std.it-college.ac.jp>
2014-10-28 11:10:36 +0900

    1028

--- Ex2.java
+++ Ex2.java
@@ -5,7 +5,8 @@ abstract class Ex {
     abstract void execute();
 }
 
-class Ex2_0 extends Ex{
+/*
+   class Ex2_0 extends Ex{
     void execute(){
         int x;
         int y;
@@ -56,7 +57,7 @@ class Ex2_2 extends Ex{
 
 class Ex2_3 extends Ex{
     void execute(){
- Scanner stdIn = new Scanner(System.in);
+        Scanner stdIn = new Scanner(System.in);
 
 
         System.out.print("整数値：");// xの値の入力を促す
@@ -118,18 +119,68 @@ class Ex2_6 extends Ex{
 
     }
 }
+*/
 class Ex2_7 extends Ex{
     void execute(){
         Random rand = new Random();
 
-        int a = rand.nextInt(10);// 0～9の乱数
-        System.out.println("random1~10:"+a);
-         }
+        int a = rand.nextInt(9)+1;// 0～9の乱数
+        System.out.println("random 0~10:"+a);
+
+        int b = rand.nextInt(9)-9;// -1~-9の乱数
+        System.out.println("random -1~-9:"+b);
+
+        int c = rand.nextInt(90)+10;// -1~-9の乱数
+        System.out.println("random 10~99:"+c);
+
+    }
+}
+class Ex2_8 extends Ex{
+    void execute(){
+
+        Scanner stdIn = new Scanner(System.in);
+        Random rand = new Random();
+
+        System.out.print("整数を入力して下さい");
+        int x = stdIn.nextInt();
+
+        int y = rand.nextInt(11)-5;
+        System.out.println(x+y);
+
+
+    }
+}
+class Ex2_9 extends Ex{
+    void execute(){
+    Random rand = new Random();
+   double a =rand.nextDouble();
+   System.out.println("0.0以上1.0未満:"+a);
+
+   double b = rand.nextDouble()+9.0;
+   System.out.println("0.0以上10.0未満"+b);
+
+   double c = rand.nextDouble()*2.0-1.0;
+   System.out.println("-1.0以上1.0未満"+c);
+
+    }
+}
+class Ex2_10 extends Ex{
+    void execute(){
+
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("姓:");
+        String x =stdIn.next();
+        System.out.println("名:");
+        String y =stdIn.next();
+        System.out.println("こんにちは"+(x+y)+"さん。");
+
+    }
 }
 public class Ex2 {
     public static void main(String[] args){
         Ex a;
-        a = new Ex2_0();
+/*        a = new Ex2_0();
         a.execute();
         a = new Ex2_1();
         a.execute();
@@ -143,7 +194,14 @@ public class Ex2 {
         a.execute();
         a = new Ex2_6();
         a.execute();
+        */
         a = new Ex2_7();
+        a.execute();
+        a = new Ex2_8();
+        a.execute();
+        a = new Ex2_9();
+        a.execute();
+        a = new Ex2_10();
         a.execute();
 
 
