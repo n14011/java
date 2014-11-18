973268444f1a8463ddcf1009902ec18035ddb725
n14011 <n14011@std.it-college.ac.jp>
2014-11-18 09:37:00 +0900

    141118

--- Ex4.java
+++ Ex4.java
@@ -1,27 +1,27 @@
 class Ex4{
 
 	public int [] ex4_1(int [] a){
-		
+
 		return a;
 
 	}
 
 	public int [] ex4_2(int [] a){
-		
+
 		return a;
 
 	}
 
 	public String [] ex4_3(int [] a){
-		
+
 		String [] hantei = {"dummy"};
-		
+
 		return hantei;
 
 	}
 
 	public int ex4_4(int a){
-		
+
 		return -1;
 
 	}
