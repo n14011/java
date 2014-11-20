79bf64b0bbb45cc7d66beb4c972b8afe6c229169
n14011 <n14011@std.it-college.ac.jp>
2014-11-20 18:27:42 +0900

    1

--- Ex4.java
+++ Ex4.java
@@ -1,7 +1,13 @@
 class Ex4{
 
 	public int [] ex4_1(int [] a){
-
+        for(int i = 1; i<a.length-1;i++){
+        if(a[0]>a[1]){
+        int tmp = a[i];
+        a[1] =a[0];
+        a[0]=tmp;
+        }
+        }
 		return a;
 
 	}
@@ -22,12 +28,13 @@ class Ex4{
 
 	public int ex4_4(int a){
 
-		return -1;
+
+		return a;
 
 	}
 
 	public int ex4_5(int a){
-		
+
 		return -1;
 
 	}
