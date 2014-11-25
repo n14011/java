3931d7c6966308745ca137a8e5b9994422bec4a6
n14011 <n14011@std.it-college.ac.jp>
2014-11-25 20:28:24 +0900

    141125

--- Ex4.java
+++ Ex4.java
@@ -70,36 +70,44 @@ class Ex4{
     public int [] ex4_6(int a){
 
         int [] ret = new int[1];
-/*        for(int i =1;i<a.length;i++){
-            if(a%i==0){
-                i++;
-            }
-
-        }*/
 
         return ret;
 
     }
 
     public boolean ex4_7(int a){
-
-        return false;
-
-    }
+     int count=0;
+     for(int i = 2;i<a;i++){
+     if(a %i == 0){
+        count++;
+     }
+     }
+     if(count > 0||a == 1){
+     return false;
+     }else {
+     return true;
+     }
+        }
 
     public int ex4_8(int [] a){
 
-        int ret = 0;
 
+        int ret = 0;
+        for(int i=0;i<a.length;i++){
+        ret+=a[i];
+        }
         return ret;
 
     }
 
     public int ex4_9(int [] a){
 
-        int ret = 0;
-
-        return ret;
-
+   double ret = 0.0;
+    for(int i=0;i<a.length;i++){
+    ret+=a[i];
+    }
+    double x = Math.round(ret/(a.length));
+    int xx = (int)x;
+    return xx;
     }
 }
--- Ex5.java
+++ Ex5.java
@@ -2,8 +2,13 @@ class Ex5{
 
 	public double ex5_1(int [] a){
 
-		double ret = 0;
+   double ret = 0.0;
+    for(int i=0;i<a.length;i++){
+    ret+=a[i];
+    }
+    double x = (ret/(a.length));
+    return x;
 
-		return ret;
+
 	}
 }
