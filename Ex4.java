0680f0b511d49ece3cfc1b3e855739d200d24e22
n14011 <n14011@std.it-college.ac.jp>
2014-11-25 11:55:36 +0900

    1125

--- Ex4.java
+++ Ex4.java
@@ -2,72 +2,104 @@ import java.util.ArrayList;
 
 class Ex4{
 
-	public int [] ex4_1(int [] a){
-        for(int i = 1; i<a.length-1;i++){
-        if(a[0]>a[1]){
-        int tmp = a[i];
-        a[1] =a[0];
-        a[0]=tmp;
-        } }
-		return a;
+    public int [] ex4_1(int [] a){
+        for(int i = 0; i < a.length-1; i++){
+                for(int j = i+1; j < a.length; j++){
+                    if(a[j] < a[i]){
+                        int temp = a[i];
+                        a[i] = a[j];
+                        a[j] = temp;
+                    }
+                }
+            }
 
-	}
+        return a;
 
-	public int [] ex4_2(int [] a){
+    }
 
-		return a;
+    public int [] ex4_2(int [] a){
+            for(int i = 0; i < a.length-1; i++){
+                for(int j = i+1; j < a.length; j++){
+                    if(a[j] > a[i]){
+                        int temp = a[i];
+                        a[i] = a[j];
+                        a[j] = temp;
+                    }
+                }
+            }
 
-	}
+        return a;
 
-	public String [] ex4_3(int [] a){
+    }
 
-		String [] hantei = {"dummy"};
-
-		return hantei;
-
-	}
-
-	public int ex4_4(int a){
+    public String [] ex4_3(int [] a){
 
 
-		return a;
+            String [] hantei = {"負","零","正"};
+            String [] b=new String[a.length];
+            for(int i=0;i<a.length;i++){
+            if(a[i]>0){
+            b[i] =hantei[2];
+            }else if(a[i]<0){
+            b[i] =hantei[0];
+            }else{
+            b[i]=hantei[1];
+            }
+            }
+            return b;
+    }
 
-	}
-
-	public int ex4_5(int a){
-
-		return -1;
-
-	}
-
-	public int [] ex4_6(int a){
-
-		int [] ret = new int[1];
-
-		return ret;
-
-	}
-
-	public boolean ex4_7(int a){
-
-		return false;
-
-	}
-
-	public int ex4_8(int [] a){
-		
-		int ret = 0;
-		
-		return ret;
-
-	}
-
-	public int ex4_9(int [] a){
-		
-		int ret = 0;
-		
-		return ret;
-
-	}
+    public int ex4_4(int a){
+       int keta=0;
+        while(0<a){
+        a /= 10;
+        keta++;
+        }
+    return keta;
 }
+    public int ex4_5(int a){
+        int sum = 0;
+        for(int i =0;i<a+1;i++){
+            sum+=i;
+        }
 
+        return sum;
+
+    }
+
+    public int [] ex4_6(int a){
+
+        int [] ret = new int[1];
+/*        for(int i =1;i<a.length;i++){
+            if(a%i==0){
+                i++;
+            }
+
+        }*/
+
+        return ret;
+
+    }
+
+    public boolean ex4_7(int a){
+
+        return false;
+
+    }
+
+    public int ex4_8(int [] a){
+
+        int ret = 0;
+
+        return ret;
+
+    }
+
+    public int ex4_9(int [] a){
+
+        int ret = 0;
+
+        return ret;
+
+    }
+}
